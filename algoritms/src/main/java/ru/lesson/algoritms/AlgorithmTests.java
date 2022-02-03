bdfc2782a86c5beac53e7d9520530bc86a0f88cf
topdevice02 <vyach.ivanov100@gmail.com>
2022-02-03 20:19:06 +0500

    Реализован алгоритм сортировки выбором

--- algoritms/src/main/java/ru/lesson/algoritms/AlgorithmTests.java
+++ algoritms/src/main/java/ru/lesson/algoritms/AlgorithmTests.java
@@ -3,11 +3,12 @@ package ru.lesson.algoritms;
 public class AlgorithmTests {
 
   public static void main(String[] args) {
-    poiskMinVMassive(new int[]{1, 2, 5, 8, 20, 32, -5, 7, 19});
-    sortByBubble(new int[]{1, 2, 5, 8, 20, 32, -5, 7, 19});
+    //poiskMinVMassive(new int[]{1, 2, 5, 8, 20, 32, -5, 7, 19});
+    //sortByBubble(new int[]{1, 2, 5, 8, 20, 32, -5, 7, 19});
+    sortViborom(new int[]{2, 5, 1, 20, 8, 32, 5, 7, 19});
   }
 
-  //Поиск минимального элемента в массиве
+  //Поиск минимального элемента в массиве------------------------
   public static void poiskMinVMassive(int[] arr) {
     int minValue = arr[0];
     int minIndex = 0;
@@ -22,7 +23,7 @@ public class AlgorithmTests {
     System.out.println("Индекс: " + minIndex);
   }
 
-  //Сортировка элементов массива методом пузырька
+  //Сортировка элементов массива методом пузырька---------------------------
   public static void sortByBubble(int[] arr) {
     boolean isSorted = false;
     while (!isSorted) {
@@ -38,6 +39,32 @@ public class AlgorithmTests {
       printArray(arr);
     }
   }
+
+  //Сортировка выбором-----------------------
+  public static int min(int[] arr, int start) {
+    int minValue = arr[start];
+    int minIndex = start;
+
+    for (int i = start + 1; i < arr.length; i++) {
+      if (arr[i] < minValue) {
+        minValue = arr[i];
+        minIndex = i;
+      }
+    }
+    return minIndex;
+  }
+
+  public static void sortViborom(int[] arr) {
+    for (int step = 0; step < arr.length; step++) {
+      printArray(arr);
+      int index = min(arr, step);
+      int tmp = arr[step];
+      arr[step] = arr[index];
+      arr[index] = tmp;
+    }
+  }
+
+
 
   private static void printArray(int[] arr) {
     for (int i = 0; i < arr.length; i++) {
